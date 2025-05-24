3ee3a5d096fb5dcbd510e20d9d1931b172b23781
antonwessel <anton62045@gmail.com>
2025-05-24 14:13:21 +0200

    Cap fps to 60

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ from constants import *
 
 def main():
     pygame.init()
+
+    # surface object
     screen = pygame.display.set_mode(
-        (SCREEN_WIDTH, SCREEN_HEIGHT))  # surface object
+        (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+
+    # delta time
+    dt = 0
 
     while True:  # game loop
-        screen.fill((0, 0, 0))  # black color
-
         for event in pygame.event.get():  # gets all user inputs or events happening
             if event.type == pygame.QUIT:
                 return  # close game
 
-        pygame.display.flip()  # refresh the screen
+        # background color
+        screen.fill("black")
+
+        # refresh the screen
+        pygame.display.flip()
+
+        # cap fps to 60
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
